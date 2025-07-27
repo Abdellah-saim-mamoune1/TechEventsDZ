9a3e33d9f9b4878e5de13f49fa2f8c045e963d7a
Abdellah-saim-mamoune1 <abdellahsaimmamoune1@gmail.com>
2025-07-27 17:01:58 +0200

    Update Section.tsx

--- src/Section.tsx
+++ src/Section.tsx
@@ -131,7 +131,7 @@ export function Section() {
           onChange={(e) => setSearch(e.target.value)}
           className="border px-3 py-2 rounded w-full md:w-1/3"
         />
-        <div className="flex gap-4">
+        <div className="flex flex-col gap-4">
             <div className="flex flex-col gap-2"> 
                 <p>From</p>
               <input type="date" value={fromDate} onChange={(e) => setFromDate(e.target.value)} className="border px-2 py-1 rounded" />
